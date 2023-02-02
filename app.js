eb2d37deda00c5bc1903e666ecfcf5cecff52628
Amdrii <andreyk557@gmail.com>
2023-02-02 16:51:24 +0200

    feat[js-stop-time-func]

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const startBtn = document.querySelector('#start')
 const screens = document.querySelectorAll('.screen')
 const timeList = document.querySelector('#time-list')
-let time = 0
+const timeEl = document.querySelector('#time')
+let time = 11
 
 startBtn.addEventListener('click', (event) => {
     event.preventDefault()
@@ -11,7 +12,36 @@ startBtn.addEventListener('click', (event) => {
 timeList.addEventListener('click', event => {
     if (event.target.classList.contains('time-btn')){
        time = parseInt(event.target.getAttribute('data-time'))
+       screens[1].classList.add('up')
        startGame()
     }
 })
+
+startGame()
+
+function startGame() {
+    setInterval(decreaseTime, 1000)
+    setTime(time)
+}
+
+function decreaseTime() {
+    if(time === 0) {
+        finishGame()
+    } else {
+        let current = --time
+        if (current < 10){
+            current = `0${current}`
+        }
+            setTime(current)
+        }
+    }
+    
+
+function setTime(value) {
+    timeEl.innerHTML = `00:${value}`
+}
+
+function finishGame() {
+
+}
 
